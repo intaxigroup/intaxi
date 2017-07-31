330729b7256a941bcea2d0f6bc5e293d38ef7524
pawlek <pavlo.voronyuk@gmail.com>
2017-07-31 22:28:53 +0200

    edit form

--- email.php
+++ email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
+    $email_to = "info@intaxi.si";
     $email_subject = "Your email subject line";
  
     function died($error) {
@@ -16,41 +16,44 @@ if(isset($_POST['email'])) {
  
  
     // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
+    if(!isset($_POST['company_name']) ||
+        !isset($_POST['company_vat']) ||
+        !isset($_POST['company_adress']) ||
+        !isset($_POST['company_user']) ||
+        !isset($_POST['company_user_email'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
  
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
+    $company_name = $_POST['company_name']; // required
+    $company_vat = $_POST['company_vat']; // required
+    $company_adress = $_POST['company_adress']; // required
+    $company_user = $_POST['company_user']; // not required
+    $company_user_email = $_POST['company_user_email']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$first_name)) {
+  if(!preg_match($string_exp,$company_name)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
+  if(!preg_match($string_exp,$company_vat)) {
     $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
  
-  if(strlen($comments) < 2) {
+  if(strlen($string_exp,$company_adress)) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+	
+  if(strlen($string_exp,$company_user)) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+	
+  if(!preg_match($string_exp,$company_user_email)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -67,11 +70,11 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Ime podjetja: ".clean_string($company_name)."\n";
+    $email_message .= "DŠ Podjetja: ".clean_string($company_vat)."\n";
+    $email_message .= "Naslov Podjetja: ".clean_string($company_adress)."\n";
+    $email_message .= "Ime in Priimek kontaktne osebe: ".clean_string($company_user)."\n";
+    $email_message .= "Email kontaktne osebe: ".clean_string($company_user_email)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
